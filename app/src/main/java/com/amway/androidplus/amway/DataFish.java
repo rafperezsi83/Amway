273d5ba973b42d6bd590ea5b84227f32d572ff53
rafperezsi83 <Rap96366689>
2017-05-16 08:39:49 -0430

    Using Parser Json and RecyclerView

---
+++ app/src/main/java/com/amway/androidplus/amway/DataFish.java
@@ -0,0 +1,10 @@
+package com.amway.androidplus.amway;
+
+public class DataFish {
+
+    public String fishImage;
+    public String fishName;
+    public String catName;
+    public String sizeName;
+    public int price;
+}
